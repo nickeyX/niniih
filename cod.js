c3b464f8499500f97e6b2eeef0f3595a5d57ebd6
nickeyX <n7ckey@gmail.com>
2025-03-09 05:23:55 -0300

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,10 +1,16 @@
 const botaoNao = document.getElementById("botao-nao");
 
-botaoNao.addEventListener("mouseover", function() {
+function moverBotao() {
     const x = Math.random() * (window.innerWidth - botaoNao.clientWidth);
     const y = Math.random() * (window.innerHeight - botaoNao.clientHeight);
-    
+
     botaoNao.style.position = "absolute";
     botaoNao.style.left = `${x}px`;
     botaoNao.style.top = `${y}px`;
-});
+}
+
+// Para computadores (mouse)
+botaoNao.addEventListener("mouseover", moverBotao);
+
+// Para celulares (toque)
+botaoNao.addEventListener("touchstart", moverBotao);
