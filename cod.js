15325b0fd7c9bb6dff9081eca4d0f7c1ec52c1fc
nickeyX <n7ckey@gmail.com>
2025-03-09 05:21:37 -0300

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,13 +1,9 @@
-document.getElementById("botao-sim").addEventListener("click", function() {
-    window.location.href = "sim.html"; // Redireciona para a nova página
-});
-
 const botaoNao = document.getElementById("botao-nao");
 
 botaoNao.addEventListener("mouseover", function() {
-    const x = Math.random() * (window.innerWidth - 100);
-    const y = Math.random() * (window.innerHeight - 50);
-
+    const x = Math.random() * (window.innerWidth - botaoNao.clientWidth);
+    const y = Math.random() * (window.innerHeight - botaoNao.clientHeight);
+    
     botaoNao.style.position = "absolute";
     botaoNao.style.left = `${x}px`;
     botaoNao.style.top = `${y}px`;
